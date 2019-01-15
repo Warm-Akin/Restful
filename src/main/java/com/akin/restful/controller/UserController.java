f94c98d9497c0c39dca23de3b529b2c059e6ebd8
Akin gan <akin.gan@oocl.com>
2019-01-15 10:59:06 +0800

    modify the UserController

--- src/main/java/com/akin/restful/controller/UserController.java
+++ src/main/java/com/akin/restful/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.akin.restful.controller;
 
+import com.akin.restful.utils.ResponseUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/restful")
 public class UserController {
+
+    @RequestMapping("/test")
+    public ResponseEntity getTest() {
+        String str = "Hello, World! This is the test~~~";
+        return ResponseUtil.success(str);
+    }
 }
